96530b4b83b4e82ead490b2f03dc84bb9667a6c0
rtsmc <93358703+rtsmc@users.noreply.github.com>
2023-09-02 15:39:34 -0400

    add vimtex and fugitive

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -139,10 +139,20 @@ _G.packer_plugins = {
     path = "/Users/ryantrubey/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/Users/ryantrubey/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
+  },
   ["vim-vsnip"] = {
     loaded = true,
     path = "/Users/ryantrubey/.local/share/nvim/site/pack/packer/start/vim-vsnip",
     url = "https://github.com/hrsh7th/vim-vsnip"
+  },
+  vimtex = {
+    loaded = true,
+    path = "/Users/ryantrubey/.local/share/nvim/site/pack/packer/start/vimtex",
+    url = "https://github.com/lervag/vimtex"
   }
 }
 

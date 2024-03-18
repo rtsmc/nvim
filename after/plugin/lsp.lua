c81b9706c0eee50a00601b21cd08affd93f00d41
rtsmc <93358703+rtsmc@users.noreply.github.com>
2024-03-17 20:13:50 -0400

    lsp changes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,6 +11,30 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end
 })
 
+--go autoformat on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        local params = vim.lsp.util.make_range_params()
+        params.context = {only = {"source.organizeImports"}}
+        -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+        -- machine and codebase, you may want longer. Add an additional
+        -- argument after params if you find that you have to write the file
+        -- twice for changes to be saved.
+        -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+        for cid, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                end
+            end
+        end
+        vim.lsp.buf.format({async = false})
+    end
+})
+
 --luasnip thing
 require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -22,7 +46,6 @@ require("mason-lspconfig").setup({
     ensure_installed = {
         --put the language servers here, see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md# for server names
         "lua_ls",
-        "eslint",
         "clangd",
         "rust_analyzer",
         "tsserver",
@@ -149,7 +172,17 @@ require("lspconfig")["tsserver"].setup {
 }
 
 require("lspconfig")["gopls"].setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    settings = {
+        gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+                unusedparams = true,
+            },
+            staticcheck = true,
+        },
+    },
 }
 
 require("lspconfig")["html"].setup {

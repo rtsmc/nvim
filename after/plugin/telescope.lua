311d0f55069362e5a5b30452a7f6ace220e62d47
rtsmc <93358703+rtsmc@users.noreply.github.com>
2024-03-23 19:34:23 -0400

    change native fzf extension, the previous one had issues on linux

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,22 +9,19 @@ require("telescope").setup({
         dynamic_preview_title = true,
     },
     extensions = {
-        fzf = {
-            fuzzy = true,                    -- false will only do exact matching
-            override_generic_sorter = true,  -- override the generic sorter
-            override_file_sorter = true,     -- override the file sorter
-            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-            -- the default case_mode is "smart_case"
-        },
         ["ui-select"] = {
             require("telescope.themes").get_dropdown {
                 -- even more opts
             }
+        },
+        fzy_native = {
+            override_generic_sorter = true;
+            override_file_sorter = true;
         }
     }
 })
-require('telescope').load_extension('fzf')
 require("telescope").load_extension("ui-select")
+require('telescope').load_extension('fzy_native')
 
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', "<leader>ff", builtin.find_files, {})

a49d6450d307ac97e58824130f739c266f77b76c
rtsmc <ryan@trubey.net>
2025-09-09 19:10:38 -0400

    simplify a bit, racket keybind only for rkt files

--- init.lua
+++ init.lua
@@ -32,31 +32,32 @@ vim.api.nvim_create_autocmd('TermOpen', {
 ----------------------------------------------------------------------------------
 -- Keybinds
 ----------------------------------------------------------------------------------
-vim.keymap.set("n", "<C-e>", vim.cmd.Oil)
+-- vim.keymap.set("n", "<C-e>", vim.cmd.Oil)
+vim.keymap.set("n", "<C-e>", vim.cmd.Ex)
 vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<C-p>", "<cmd>cprev<CR>")
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
 vim.keymap.set("n", '<leader>qf', vim.diagnostic.setqflist);
 
--- small terminal
-vim.keymap.set("n", "<space>st",
-    function()
-        vim.cmd.vnew()
-        vim.cmd.term()
-        vim.cmd.wincmd("J")
-        vim.api.nvim_win_set_height(0, 15)
-    end)
+local function small_term (cmd)
+    vim.cmd.vnew()
+    vim.cmd.term(cmd)
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 15)
+end
 
-vim.keymap.set("n", "<leader>r",
-    function()
-        local filename = vim.fn.expand('%:t')
-        local escaped_filename = vim.fn.shellescape(filename)
-        vim.cmd.vnew()
-        vim.cmd.term("racket -e '(enter! \""..escaped_filename.."\")' -i")
-        vim.cmd.wincmd("J")
-        vim.api.nvim_win_set_height(0, 15)
-    end)
+-- small terminal
+vim.keymap.set("n", "<space>st", small_term)
+
+-- racket language keybindings
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "rkt",
+    callback = function()
+        local escaped_filename = vim.fn.shellescape(vim.fn.expand('%:t'))
+        vim.keymap.set("n", "<leader>r", function() small_term(escaped_filename) end)
+    end
+})
 
 -- formatting keybind
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -77,16 +78,45 @@ vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
 -- Plugins
 ----------------------------------------------------------------------------------
 vim.pack.add({
-    { src = "https://github.com/rebelot/kanagawa.nvim" },
-    { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/echasnovski/mini.icons" },
-    { src = "https://github.com/echasnovski/mini.pick" },
+    { src = 'https://github.com/rebelot/kanagawa.nvim' },
+    { src = 'https://github.com/nvim-mini/mini.pick' },
+    { src = 'https://github.com/nvim-mini/mini.icons' },
+    { src = 'https://github.com/neovim/nvim-lspconfig' }
 })
 
 vim.cmd.colorscheme "kanagawa"
 
-require "oil".setup()
 require "mini.icons".setup()
 require "mini.pick".setup()
 
 vim.lsp.enable({ "lua_ls", "clangd", "basedpyright", "ruff", "racket_langserver" })
+
+vim.lsp.config("basedpyright", {
+    settings = {
+        basedpyright = {
+            disableOrganizeImports = true,
+            analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = 'openFilesOnly',
+                diagnosticSeverityOverrides = {
+                    reportUnusedVariable = "hint",
+                    reportUnusedImport = "hint",
+                    reportUnusedClass = "hint",
+                    reportUnusedFunction = "hint",
+                    reportPrivateImportUsage = "none",
+                    reportPrivateLocalImportUsage = "none",
+                    reportMissingTypeStubs = "none",
+                },
+            },
+        },
+    }
+})
+
+vim.lsp.config("ruff", {
+    settings = {
+        lint = {
+            enable = false
+        }
+    }
+})
